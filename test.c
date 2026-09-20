94d192a84b8079b2ca7cb2e4007939bbc7e02429
Arsalan Arish <mail@arsalanarish.com>
2026-09-20 15:00:24 +0500

    .

--- test.c
+++ test.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 #include <stdbool.h>
+#include <math.h>
 
 typedef unsigned char u8;
 
 void printb(u8* data, size_t size, bool little_endian) {
     for (int i = 0; i < size; i++) {
-        u8 byte = little_endian ? data[size - i - 1] : data[i];
+        u8 byte = little_endian ? data[i] : data[size - i - 1];
         for (int j = 7; j >= 0; j--) {
             printf("%d", (byte >> j) & 1);
         }
@@ -16,6 +17,17 @@ void printb(u8* data, size_t size, bool little_endian) {
 
 
 int main() {
-    char str[] = "hello";
-    printb(str, sizeof(str), false);
+    /*  There are 2 fundamental numeric data types in C:
+    *       int & float, and their variations of different sizes
+    *
+    *   Others are types for specific behavior:
+    *       pointers for manipulating with addresses
+            _Bool for restricting a 1-byte int to only 2 values
+            A built-in feature that maps a single-quoted charater to its ascii int value
+            A built-in feature that converts string literals to \0-terminated character arrays (stored in .rodata)
+            Arrays for contiguous storage of data
+            Structs to group data
+            Unions & Enums for specific behavior with data types
+    */
+    
 }
